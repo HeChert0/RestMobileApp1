59ba1b98f9994f3b7c138f63b6f280d9186b9a08
HeChert <nikitamaslakov36@gmail.com>
2025-02-24 14:54:10 +0300

    added pon

--- src/main/java/app/controller/PhoneController.java
+++ src/main/java/app/controller/PhoneController.java
@@ -3,7 +3,14 @@ package app.controller;
 import app.entities.Phone;
 import app.service.PhoneService;
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 @RequestMapping("/phones")
